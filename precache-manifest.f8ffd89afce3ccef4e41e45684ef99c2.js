d1672b3101137a5b92e02e1b871d1e168a53de6b
Gringo <trand0161@gmail.com>
2019-06-30 20:36:35 -0500

    Updates

---
+++ precache-manifest.f8ffd89afce3ccef4e41e45684ef99c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08475801dc27a9950036d87e49de6a46",
+    "url": "/Top-gun-final-proyect/index.html"
+  },
+  {
+    "revision": "3b33c8e5fa452aedcfd7",
+    "url": "/Top-gun-final-proyect/static/css/2.43d354e1.chunk.css"
+  },
+  {
+    "revision": "4d34f1e694e4241e4e61",
+    "url": "/Top-gun-final-proyect/static/css/main.ecd8e540.chunk.css"
+  },
+  {
+    "revision": "3b33c8e5fa452aedcfd7",
+    "url": "/Top-gun-final-proyect/static/js/2.c61f6ded.chunk.js"
+  },
+  {
+    "revision": "4d34f1e694e4241e4e61",
+    "url": "/Top-gun-final-proyect/static/js/main.42f7b0e9.chunk.js"
+  },
+  {
+    "revision": "61edc5b9711d9eca57a9",
+    "url": "/Top-gun-final-proyect/static/js/runtime~main.4d5f1c54.js"
+  }
+]);
